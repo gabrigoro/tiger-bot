35c70eba4e9608b6c549de4bd29efcda64c209a1
franelfers <fransozzan@gmail.com>
2022-12-15 00:28:37 -0300

    update

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Telegraf, Context } from 'telegraf'
+import { Telegraf, Context, Markup } from 'telegraf'
 import fetch from 'node-fetch'
 import store, { Collection } from './store'
 import dotenv from 'dotenv'
@@ -27,16 +27,90 @@ bot.start(async (context) => {
         if (hours === 7 && minute === 12) context.reply('Son las 7 y 12 che')
     }, MINUTO)
 })
-bot.command('hello', (ctx) => ctx.reply('Adfsfase'))
+
+let target_id = 0
+let amount_id = 0
+
+bot.command('pago', (ctx) => {
+    const fran = {
+        text: 'Fulano',
+        callback_data: 'fulano'
+    }
+    ctx.reply('Elegir la categoria del pago', {
+        reply_markup: {
+            inline_keyboard: [[fran, fran,fran]],
+        }
+    })
+})
+bot.on('callback_query', async (ctx) => {
+    const { data } = (await ctx.callbackQuery) as {data:string}
+
+    if (data === 'fulano') return ctx.reply('Que monto pagaste?', {
+        reply_markup: {
+            force_reply: true,
+            one_time_keyboard: true,
+        }
+    }).then((a) => {
+        amount_id = a.message_id
+    })
+    
+    if (data === 'guardar') return ctx.reply('💸')
+})
+
+let amount = 0
+let target = ''
+
+
+bot.on('text', (ctx) => {
+    const { text, reply_to_message } = ctx.message
+    const reply_id = reply_to_message?.message_id || 0
+
+    if (reply_id === amount_id) {
+        amount = parseInt(text) || 0
+        return ctx.reply('Destino?', {
+            reply_markup: {
+                force_reply: true,
+                one_time_keyboard: true,
+            }
+        }).then((a) => {
+            target_id = a.message_id
+        })
+    }
+
+    if (reply_id === target_id) {
+        target = text
+        return ctx.reply(`Pagaste ${amount} a ${target} ✔`, {
+            reply_markup: {
+                inline_keyboard: [[{
+                    text: 'Guardar',
+                    callback_data: 'guardar'
+                }]]
+            }
+        })
+    }
+})
+
 bot.on('sticker', ctx => ctx.reply('No me envies stickers no los entiendo'))
-bot.on('text', (ctx) => processMessage(ctx.message.text, (txt) => {ctx.reply(txt)}))
-bot.launch().then(() => {console.log('Bot launched')})
+/**
+ * https://core.telegram.org/bots/features#global-commands
+ * Telegram recomienda usar `Global Commands` para
+ * interacciones consistentes para bots. 
+ * /start, /help y /settings
+ */
+
+/**
+ * Agrega los comandos a un menu facil de usar.
+ * El comando tiene que empezar con una `/`, tiene
+ * que existir y tiene que declararse de antemano.
+ */
 bot.telegram.setMyCommands([
     {
-        command: '/hello',
-        description: 'Hola!'
+        command: '/pago',
+        description: 'Anotar un pago'
     }
 ])
+
+bot.launch()
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
